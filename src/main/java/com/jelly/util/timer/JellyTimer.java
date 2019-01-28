c73fb7b1e0a860328bf16eb7b6046c36d7f5c7a3
dongxiaohong <dongxiaohong@daokoudai.com>
2019-01-28 11:56:00 +0800

    简单的定时器modify

--- src/main/java/com/jelly/util/timer/JellyTimer.java
+++ src/main/java/com/jelly/util/timer/JellyTimer.java
@@ -3,6 +3,8 @@ package com.jelly.util.timer;
 import org.apache.commons.lang.time.DateFormatUtils;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 定时器
@@ -41,8 +43,21 @@ public class JellyTimer {
         }
     }
 
+    /**Timer方式*/
+    public static void gTimer(){
+        Timer timer = new Timer("Test-Timer");
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("######定时器执行方式######");
+            }
+        };
+        /**每5秒执行一次*/
+        timer.schedule(task,new Date(),5*1000);
+    }
     /**主方法*/
     public static void main(String[] args) {
-        poll();
+//        poll();
+        gTimer();
     }
 }
