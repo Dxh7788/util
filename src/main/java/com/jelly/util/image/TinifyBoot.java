6d5026c5e6a68c8456c7795b71cdcc48ba976ad0
Dxh7788 <anan1215>
2019-05-06 11:36:42 +0800

    TinifyJpg压缩测试

---
+++ src/main/java/com/jelly/util/image/TinifyBoot.java
@@ -0,0 +1,23 @@
+package com.jelly.util.image;
+
+import com.jelly.util.watch.TimeWatch;
+import com.tinify.Source;
+import com.tinify.Tinify;
+
+import java.io.IOException;
+
+/**
+ * @author dongxiaohong
+ * @date 2019/5/6 11:28
+ */
+public class TinifyBoot {
+    private static String srcFileName="/Users/dongxiaohong/Pictures/2018-10-02161343.jpg";
+    private static String distFileName="/Users/dongxiaohong/Pictures/2018-10-02161343_tiny_01.jpg";
+    public static void main(String[] args) throws IOException {
+        TimeWatch.start();
+        Tinify.setKey("qLtE6CKG9Skamd1SHiPSGACkvw7icuU6");
+        Source source = Tinify.fromFile(srcFileName);
+        source.toFile(distFileName);
+        TimeWatch.end();
+    }
+}
